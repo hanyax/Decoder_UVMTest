045d2d48dee5286c25bc082bd85a622aeca25ba8
Shawn Xu <hanyax@cs.washington.edu>
2020-06-11 00:25:46 -0700

    Started

--- decryptor_tb.sv
+++ decryptor_tb.sv
@@ -138,6 +138,7 @@ class gen_item_seq extends uvm_sequence;
         `uvm_info("SEQ", $sformatf("Generate new item: %s", m_item.convert2str()), UVM_LOW)
         finish_item(m_item);
       end
+
       `uvm_info("SEQ", $sformatf("Done generation of %0d items", num), UVM_LOW)
   endtask
 endclass
@@ -161,6 +162,7 @@ class driver extends uvm_driver #(reg_item);
 
   virtual task run_phase(uvm_phase phase);
     super.run_phase(phase);
+    vif.init <= 'b1;'
     forever begin
       reg_item m_item;
       `uvm_info("DRV", $sformatf("Wait for item from sequencer"), UVM_LOW)
@@ -168,20 +170,20 @@ class driver extends uvm_driver #(reg_item);
       drive_item(m_item);
       seq_item_port.item_done();
     end
+    vif.wr_en <= 'b0;
+    vif.init <= 'b0;
   endtask
 
   virtual task drive_item(reg_item m_item);
-      vif.sel <= 1;
-      vif.addr 	<= m_item.addr;
-      vif.wr 	<= m_item.wr;
+      vif.wr_en <= m_item.wr_en;
+      vif.waddr <= m_item.waddr;
       vif.wdata <= m_item.wdata;
       @ (posedge vif.clk);
-      while (!vif.ready)  begin
-        `uvm_info("DRV", "Wait until ready is high", UVM_LOW)
+      while (!vif.done)  begin
+        `uvm_info("DRV", "Wait until done is high", UVM_LOW)
         @(posedge vif.clk);
       end
 
-      vif.sel <= 0;
   endtask
 endclass
 
@@ -214,19 +216,18 @@ class monitor extends uvm_monitor;
     // transaction and writes into analysis port when complete
     forever begin
       @ (posedge vif.clk);
-      if (vif.sel) begin
-        reg_item item = new;
-        item.addr = vif.addr;
-        item.wr = vif.wr;
-        item.wdata = vif.wdata;
+      reg_item item = new;
+      item.raddr = vif.raddr;
+      item.waddr = vif.waddr;
+      item.wr_en = vif.wr_en;
+      item.wdata = vif.wdata;
 
-        if (!vif.wr) begin
-          @(posedge vif.clk);
-        	item.rdata = vif.rdata;
-        end
-        `uvm_info(get_type_name(), $sformatf("Monitor found packet %s", item.convert2str()), UVM_LOW)
-        mon_analysis_port.write(item);
-      end
+      @(posedge vif.clk);
+    	item.rdata = vif.rdata;
+
+      `uvm_info(get_type_name(), $sformatf("Monitor found packet %s", item.convert2str()), UVM_LOW)
+      mon_analysis_port.write(item);
+
     end
   endtask
 endclass
@@ -250,30 +251,11 @@ class scoreboard extends uvm_scoreboard;
   endfunction
 
   virtual function write(reg_item item);
-     if (item.wr) begin
-        if (refq[item.addr] == null)
-          refq[item.addr] = new;
-
-        refq[item.addr] = item;
-       `uvm_info(get_type_name(), $sformatf("Store addr=0x%0h wr=0x%0h data=0x%0h", item.addr, item.wr, item.wdata), UVM_LOW)
-      end
-
-        if (!item.wr) begin
-          if (refq[item.addr] == null)
-            if (item.rdata != 'h1234)
-              `uvm_error (get_type_name(), $sformatf("First time read, addr=0x%0h exp=1234 act=0x%0h",
-                                                     item.addr, item.rdata))
-          	else
-              `uvm_info(get_type_name(), $sformatf("PASS! First time read, addr=0x%0h exp=1234 act=0x%0h",
-                                                   item.addr, item.rdata), UVM_LOW)
-          else
-            if (item.rdata != refq[item.addr].wdata)
-              `uvm_error (get_type_name(), $sformatf("addr=0x%0h exp=0x%0h act=0x%0h",
-                                                    item.addr, refq[item.addr].wdata, item.rdata))
-           else
-             `uvm_info(get_type_name(), $sformatf("PASS! addr=0x%0h exp=0x%0h act=0x%0h",
-                       item.addr, refq[item.addr].wdata, item.rdata), UVM_LOW)
-        end
+    if (item.rdata != refq[item.addr].wdata) begin
+      `uvm_error (get_type_name(), $sformatf("addr=0x%0h exp=0x%0h act=0x%0h", item.raddr, item.wdata, item.rdata))
+    else
+      `uvm_info(get_type_name(), $sformatf("PASS! addr=0x%0h exp=0x%0h act=0x%0h", item.raddr, item.wdata, item.rdata), UVM_LOW)
+    end
   endfunction
 endclass
 
@@ -350,7 +332,7 @@ class test extends uvm_test;
   virtual task run_phase(uvm_phase phase);
     gen_item_seq seq = gen_item_seq::type_id::create("seq");
     phase.raise_objection(this);
-    apply_reset();
+    //apply_reset();
 
     seq.randomize() with {num inside {[20:30]}; };
     seq.start(e0.a0.s0);
@@ -358,12 +340,14 @@ class test extends uvm_test;
     phase.drop_objection(this);
   endtask
 
+  /*
   virtual task apply_reset();
     vif.rstn <= 0;
     repeat(5) @ (posedge vif.clk);
     vif.rstn <= 1;
     repeat(10) @ (posedge vif.clk);
   endtask
+  */
 endclass
 
 // The interface allows verification components to access DUT signals
@@ -373,8 +357,8 @@ interface reg_if (input bit clk);
   logic wr_en;
   logic [7:0] raddr;
   logic [7:0] waddr;
-  logic [7:0] data_in;
-  logic [7:0] data_out;
+  logic [7:0] rdata;
+  logic [7:0] wdata;
   logic done;
 
 endinterface
@@ -387,14 +371,15 @@ module tb;
   always #10 clk =~ clk;
   reg_if _if (clk);
 
-  reg_ctrl u0 ( .clk (clk),
-            .addr (_if.addr),
-               .rstn(_if.rstn),
-            .sel  (_if.sel),
-               .wr (_if.wr),
-            .wdata (_if.wdata),
-            .rdata (_if.rdata),
-            .ready (_if.ready));
+  top_level_4_260 dut (
+    .clk(clk),
+    .init(_if.init),
+    .wr_en(_if.wr_en),
+    .raddr(_if.wr_en),
+    .waddr(_if.waddr),
+    .data_in(_if.data_in),
+    .data_out(_if.data_out),
+    .done(_if.done) );
 
   test t0;
 
